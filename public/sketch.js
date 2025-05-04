90f090babcf50de97f8cfffb34e83a3a0c3a4bb9
Adithya <adithya181209@gmail.com>
2025-05-04 10:46:00 +0800

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,8 @@
-function setup() {
-  createCanvas(400, 400);
-}
-
-function draw() {
-  background(220);
-}
+document.querySelectorAll('#ussr-map path').forEach(e => {
+  e.onmouseenter = () => {
+    e.classList.add('in');
+  }
+  e.onmouseleave = () => {
+    e.classList.remove('in');
+  }
+})
